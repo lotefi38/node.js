e3412c8f1dda51e493783ac1b691da0c4d7046a5
lotefi38 <lotefimohdeb@outlook.fr>
2024-05-07 13:10:37 +0200

    second commit

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express();
 
 const port = 3000;
 
+app.get('/movies', (req, res) => {
+     res.send('bientot des filmes ici');
+});
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`film numéro ${id}`)
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 
-})
+});
