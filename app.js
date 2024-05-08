fd6ece48822c7b6f8f6497b5dd38b2c657c7bf5f
lotefi38 <lotefimohdeb@outlook.fr>
2024-05-08 13:56:48 +0200

    third commit

--- app.js
+++ app.js
@@ -3,17 +3,35 @@ const app = express();
 
 const port = 3000;
 
+app.use('/public', express.static('public'));
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+//app.get('/movies-details', (req, res) => {
+ //      res.render('movies-details')
+//})
+
 app.get('/movies', (req, res) => {
-     res.send('bientot des filmes ici');
+     //res.send('bientot des filmes ici');
+     res.render('movies')
 });
+
+app.get('/movies/add', (req, res) =>{
+    res.send('prochainement, un formulaire d\'ajout ici ' );
+ });
 
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
-    res.send(`film numéro ${id}`)
+    //res.send(`film numéro ${id}`)
+    res.render('movies-details')
+//})
 });
 
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index');
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
